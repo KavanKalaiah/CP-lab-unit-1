fba261cdad9a452e47638e9e50d45c548bcb2158
KavanKalaiah <kavankalaiahpd@gmail.com>
2025-11-18 17:34:18 +0530

    Add files via upload

---
+++ P4d.c
@@ -0,0 +1,16 @@
+//(4d)c program to finding positivity of the number
+#include<stdio.h>
+void main()
+{
+    int a;
+    printf("enter a");
+    scanf("%d",&a);
+    if(a>0)
+        printf("a is positive ");
+    
+}
+
+output
+
+enter a23
+a is positive 
