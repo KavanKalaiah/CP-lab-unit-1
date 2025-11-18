f31b4ec67dbfb7fa6fcaab17955f969e71007db3
KavanKalaiah <kavankalaiahpd@gmail.com>
2025-11-18 17:32:33 +0530

    Add files via upload

---
+++ P4a.c
@@ -0,0 +1,18 @@
+//(4a)c program to check positive number
+#include<stdio.h>
+int main()
+{
+    int a;
+    printf("enter a");
+    scanf("%d",&a);
+    if(a>0)
+    {
+        printf("a ia positive no.%d");
+    }
+    printf("done");
+    return 0;
+}
+
+output
+enter a34
+a ia positive no.34done
