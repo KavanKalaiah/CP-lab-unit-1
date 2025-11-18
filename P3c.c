0cb19eee16d0df15c7571ba6718f4811729d6208
KavanKalaiah <kavankalaiahpd@gmail.com>
2025-11-18 17:29:13 +0530

    Add files via upload

---
+++ P3c.c
@@ -0,0 +1,17 @@
+//(3c) increment or decrement operator
+
+#include<stdio.h>
+void main()
+{
+    int a=10;//to increment\decrement
+    printf("a++=%d\n",a++);
+    printf("a--=%d\n",a--);
+    printf("--a=%d\n",--a);
+    printf("++a=%d",++a);
+}
+
+output
+a++=10
+a--=11
+--a=9
+++a=10
