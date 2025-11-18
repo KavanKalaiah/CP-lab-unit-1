455007dfc2c437c6d55f503dd36947d0d4ce9325
KavanKalaiah <kavankalaiahpd@gmail.com>
2025-11-18 17:30:31 +0530

    Add files via upload

---
+++ P3e.c
@@ -0,0 +1,18 @@
+//(3e) logical operator program
+
+#include<stdio.h>
+void main()
+{
+    int a=0,b=5;
+    printf("a&&b %d\n",a&&b);
+    printf("a||b %d\n",a||b);
+    printf("!a %d\n",!a);
+    printf("!b %d\n",!b);
+}
+
+output 
+a&&b 0
+a||b 1
+!a 1
+!b 0
+
